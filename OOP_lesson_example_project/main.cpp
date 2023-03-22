60b9c85b361572ad3d3b925c1b5ceee135997fd6
KANTNOLI <zahargysev02@gmail.com>
2023-03-22 17:56:42 +0300

    Add function get_best_student

--- OOP_lesson_example_project/main.cpp
+++ OOP_lesson_example_project/main.cpp
@@ -33,6 +33,10 @@ public:
 //
 //}
 
+Student get_best_student(Student st1, Student st2, Student st3) {
+	
+	return st1.avg_mark > st2.avg_mark && st1.avg_mark > st3.avg_mark ? st1 : st2.avg_mark > st3.avg_mark ? st2 : st3;
+}
 
 
 int main() {
@@ -42,12 +46,14 @@ int main() {
 	Student st3;
 
 	st1.init("ivan", "ivanov", 14, 10);
-	st2.init("boba", "Buba", 14, 10);
-	st3.init("Huma", "Homa", 14, 10);
+	st2.init("boba", "Buba", 14, 13);
+	st3.init("Huma", "Homa", 14, 5);
 	
 	cout << st1.convert_to_string() << endl;
 	cout << st2.convert_to_string() << endl;
 	cout << st3.convert_to_string() << endl;
+
+	cout << "Best student: " << get_best_student(st1, st2, st3).convert_to_string() << endl;
 
 	//Student st2 = st1;
 
